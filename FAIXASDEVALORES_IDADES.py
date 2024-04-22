b007270895def3d73e4cf1a14cec796f61efa6db
Elisson Chaves 🤖 <148161410+myElonChaves565@users.noreply.github.com>
2024-04-22 10:45:58 -0300

    Add files via upload

---
+++ FAIXASDEVALORES_IDADES.py
@@ -0,0 +1,19 @@
+idade = int (input("Digite sua idade: "))
+
+if idade >= 0 and idade <= 11:
+    print("Criança")
+
+elif idade >= 12 and idade <= 18:
+    print("Adolescente")
+
+elif idade >= 19 and idade <= 24:
+    print("Jovem")
+
+elif idade >= 25 and idade <= 40:
+    print("Adulto")
+
+elif idade >= 41 and idade <= 60:
+    print("Meia Idade")
+
+elif idade > 60:
+    print("Idoso")
